e5a78e14a9522785700277799502f2e1ef6cd5f8
riinaintsar <165931463+riinaintsar@users.noreply.github.com>
2024-04-03 19:19:27 +0300

    Create main.java

---
+++ main.java
@@ -0,0 +1,24 @@
+public class Main {
+  public static void main(String[] args) {
+
+   // +5 wear super warm 
+    // +5 to 15 warm
+    // 15 to 30 normal 
+    // 30+ cooling 
+    
+    double temp = -15.0;
+
+    if (temp <= 5) {
+      System.out.println("Wear super warm");
+    }
+    else if (temp <= 15) {
+      System.out.println("Wear warm");
+    }
+    else if (temp <= 30) {
+      System.out.println("Wear normal");
+    }
+    else {
+      System.out.println("You need cooling");
+    }
+}
+}
